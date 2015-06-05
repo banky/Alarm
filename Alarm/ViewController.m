a70f57f252f1cb5b0c9a9ecbcbc0a1bf8e84022b
bankya <bankiadebajo@hotmail.com>
2015-06-04 22:48:02 -0400

    Add tableView for list of alarms and a page for adding alarms

--- Alarm/ViewController.m
+++ Alarm/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () //<UITableViewDelegate>
 
 @end
 
@@ -17,8 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //self.tableView.delegate = self;
     
-    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,6 +26,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
 
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *tableIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:tableIdentifier];
+    }
+
+    cell.textLabel.text = [NSString stringWithFormat:@"%li", (long)indexPath.row];
+    return cell;
+}
 @end
